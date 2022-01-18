3e842f713b142f4c3f47671852622091bb08f33d
juliakonoval <yulyakonoval1998@gmail.com>
2022-01-18 20:06:54 +0200

    note window created

--- lib/pages/notes.dart
+++ lib/pages/notes.dart
@@ -1,38 +1,40 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 class Notes extends StatefulWidget {
- const Notes({Key? key}) : super (key: key);
+  const Notes({Key? key}) : super(key: key);
 
- @override
- _NotesState createState() => _NotesState();
+  @override
+  _NotesState createState() => _NotesState();
 }
 
 class _NotesState extends State<Notes> {
-
   String noteText = '';
   List notes = [];
 
-void _openEditor() {
-  Navigator.of(context).push(
-    MaterialPageRoute(builder: (BuildContext context) {
+  void _openEditor() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
       return Scaffold(
-        appBar: AppBar(
-          title: Text('Notes'),
-          backgroundColor: Colors.purple[900],
+          appBar: AppBar(
+            title: Text('Notes'),
+            backgroundColor: Colors.purple[900],
           ),
-        body: Row(children: [
-          ElevatedButton(onPressed: () {
-            Navigator.pop(context);
-            Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
-          }, 
-          child: Text('Back')),
-          Padding(padding: EdgeInsets.only(left: 15)),
-          Text('Our simple menu')
-        ],)
-      );
-    })
-  );
-}
+          body: Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, '/', (route) => false);
+                  },
+                  child: Text('Back')),
+              Padding(padding: EdgeInsets.only(left: 15)),
+              Text('Our simple menu')
+            ],
+          ));
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,75 +42,104 @@ void _openEditor() {
       appBar: AppBar(
         title: Text('Notes'),
         backgroundColor: Colors.purple[900],
-        actions: [
-          IconButton(onPressed: _openEditor, icon: Icon(Icons.note))
-        ],
+        actions: [IconButton(onPressed: _openEditor, icon: Icon(Icons.note))],
       ),
       body: ListView.builder(
-        itemCount: notes.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Dismissible(
-            key: Key(notes[index]),
-            child: Card(
-              child: ListTile(title: Text(notes[index]),
-              trailing: IconButton(
-                icon: Icon(
-                 Icons.delete_sweep,
-                 color: Colors.purple[900],
+          itemCount: notes.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+              key: Key(notes[index]),
+              child: Card(
+                child: ListTile(
+                  title: Text(notes[index]),
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete_sweep,
+                      color: Colors.purple[900],
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        notes.removeAt(index);
+                      });
+                    },
+                  ),
                 ),
-              onPressed: () {
+              ),
+              onDismissed: (direction) {
                 setState(() {
                   notes.removeAt(index);
                 });
               },
-            ),
-          ),
-        ),
-        onDismissed: (direction) {
-          setState(() {
-            notes.removeAt(index);
-          });
-        },
-      );
-    }
-  ),
-  floatingActionButton: FloatingActionButton(
-    backgroundColor: Colors.purple[900],
-    onPressed: () {
-      showDialog(context: context, builder: (BuildContext context) {
-        return AlertDialog(
-          insetPadding: EdgeInsets.zero,
-          contentPadding: EdgeInsets.all(120),
-          title: Text('Add note', style: TextStyle(color: Colors.purple[900], )),
-          content: TextField(
-            decoration: InputDecoration.collapsed(hintText: "Enter your text here"),
-            onChanged: (String value) {
-              noteText = value;
-            },
-          ),
-          actions: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.purple[900]
-              ), 
-              onPressed: () {
-              setState(() {
-                notes.add(noteText);
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple[900],
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  insetPadding: EdgeInsets.zero,
+                  alignment: Alignment.topCenter,
+                  title: Text('Add note',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.purple[900],
+                        fontSize: 23,
+                        fontWeight: FontWeight.w500,
+                      )),
+                  content: Builder(builder: (context) {
+                    var height = MediaQuery.of(context).size.height;
+                    var width = MediaQuery.of(context).size.width;
+                    return Container(
+                      alignment: Alignment.topLeft,
+                      height: height - 60,
+                      width: width - 80,
+                      child: TextField(
+                        keyboardType: TextInputType.multiline,
+                        minLines: null,
+                        maxLines: null,
+                        cursorColor: Colors.purple[900],
+                        decoration: new InputDecoration(
+                          alignLabelWithHint: true,
+                          border: new OutlineInputBorder(
+                            borderSide: new BorderSide(color: Colors.black12),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5.0)),
+                              borderSide: BorderSide(color: Colors.black12)),
+                          hintText: 'Enter text here',
+                        ),
+                        onChanged: (String value) {
+                          noteText = value;
+                        },
+                      ),
+                    );
+                  }),
+                  actions: [
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.purple[900]),
+                        onPressed: () {
+                          setState(() {
+                            if (noteText != '') {
+                              notes.add(noteText);
+                            }
+                            log('data: $noteText');
+                          });
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Save'))
+                  ],
+                );
               });
-              Navigator.of(context).pop();
-            }, 
-            child: 
-            Text('Save')
-            )
-          ],
-        );
-      });
-    },
-    child: Icon(
-      Icons.add_box,
-      color: Colors.white,
-    ),
-  ),
-  );
+        },
+        child: Icon(
+          Icons.add_box,
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
