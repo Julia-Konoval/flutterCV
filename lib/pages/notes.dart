39a19d3b3d04f1e8381db02236f4ef7b037275a6
juliakonoval <yulyakonoval1998@gmail.com>
2022-01-25 15:03:52 +0200

    editing updates

--- lib/pages/notes.dart
+++ lib/pages/notes.dart
@@ -11,6 +11,7 @@ class Notes extends StatefulWidget {
 class _NotesState extends State<Notes> {
   String noteText = '';
   List notes = [];
+  var textController = new TextEditingController();
 
   void _openEditor() {
     Navigator.of(context)
@@ -52,6 +53,78 @@ class _NotesState extends State<Notes> {
               child: Card(
                 child: ListTile(
                   title: Text(notes[index]),
+                  leading: IconButton(
+                    icon: Icon(
+                      Icons.edit,
+                      color: Colors.purple[900],
+                    ),
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              insetPadding: EdgeInsets.zero,
+                              alignment: Alignment.topCenter,
+                              title: Text(
+                                'Edit note',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.purple[900],
+                                  fontSize: 23,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              content: Builder(builder: (context) {
+                                var height = MediaQuery.of(context).size.height;
+                                var width = MediaQuery.of(context).size.width;
+                                return Container(
+                                  alignment: Alignment.topLeft,
+                                  height: height - 60,
+                                  width: width - 80,
+                                  child: new TextField(
+                                    controller: textController,
+                                    keyboardType: TextInputType.multiline,
+                                    minLines: null,
+                                    maxLines: null,
+                                    cursorColor: Colors.purple[900],
+                                    decoration: new InputDecoration(
+                                      alignLabelWithHint: true,
+                                      border: new OutlineInputBorder(
+                                        borderSide: new BorderSide(
+                                            color: Colors.black12),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(5.0)),
+                                          borderSide: BorderSide(
+                                              color: Colors.black12)),
+                                      hintText: 'Enter text here',
+                                    ),
+                                    onChanged: (String value) {
+                                      textController.text = value;
+                                      value = noteText;
+                                    },
+                                  ),
+                                );
+                              }),
+                              actions: [
+                                ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        primary: Colors.purple[900]),
+                                    onPressed: () {
+                                      setState(() {
+                                        if (noteText != '') {
+                                          notes.add(noteText);
+                                        }
+                                      });
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: Text('Save'))
+                              ],
+                            );
+                          });
+                    },
+                  ),
                   trailing: IconButton(
                     icon: Icon(
                       Icons.delete_sweep,
